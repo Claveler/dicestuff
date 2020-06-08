b82df5ff366e2656c7e5763744142bd9417ac538
Claveler <claveler@hotmail.com>
2020-06-08 01:35:52 -0400

    yeah bro, commi!

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
+          title: Text('Roll da dice'),
           backgroundColor: Colors.red,
         ),
         body: DicePage(),
@@ -18,6 +18,32 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    int leftDieNumber;
+    leftDieNumber = 1;
+
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              child: Image.asset('images/dice$leftDieNumber.png'),
+              onPressed: () {
+                print('You pressed the left die');
+                leftDieNumber = leftDieNumber + 1;
+                print(leftDieNumber);
+              },
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              child: Image.asset('images/dice4.png'),
+              onPressed: () {
+                print('You pressed the right die');
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
